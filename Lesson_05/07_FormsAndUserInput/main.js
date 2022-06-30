583673a4cf709e4791bc6509a9146abdb0ed7c5d
Joshua J. Haller <phone@joshuajhaller.com>
2022-06-30 10:35:30 +1000

    Finished the FormsAndUserInput video. Really interesting to use the different v-binds.

--- Lesson_05/07_FormsAndUserInput/main.js
+++ Lesson_05/07_FormsAndUserInput/main.js
@@ -1,9 +1,23 @@
 const app = Vue.createApp({
   data() {
     return {
-      text: '',
-      checked: true,
-      city: ''
+      comment: null,
+      comments: [],
+      error: null
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.comment) {
+        let newComment = this.comment;
+        this.comments.push(newComment);
+        this.comment = null;
+        if (this.error) {
+          this.error = null;
+        }
+      } else {
+        this.error = "Put a comment in you dork!";
+      }
     }
   }
 
